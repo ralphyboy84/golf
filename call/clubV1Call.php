c87b319ee0ed5adb28d1f84999ac5ac6415fc5a7
Ralph <ralph@ralphwardlaw.co.uk>
2026-01-12 19:52:52 +0000

    Refactoring

--- call/clubV1Call.php
+++ call/clubV1Call.php
@@ -4,7 +4,7 @@ require_once "Call.php";
 
 class ClubV1Call extends Call
 {
-    public function getTeeTimesForDay($date, $club, $courseId = false)
+    private function _doCurlCall($url)
     {
         $ch = curl_init();
         curl_setopt_array($ch, [
@@ -12,46 +12,28 @@ class ClubV1Call extends Call
             CURLOPT_USERAGENT => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36",
         ]);
         curl_setopt($ch, CURLOPT_HEADER, true);
-        curl_setopt(
-            $ch,
-            CURLOPT_URL,
-            "https://$club.hub.clubv1.com/Visitors/TeeSheet?date=$date",
-        );
+        curl_setopt($ch, CURLOPT_URL, $url);
         $server_output = curl_exec($ch);
         return preg_replace("/^HTTP\/2\s+200\s.*?\R\R/s", "", $server_output);
+    }
+    public function getTeeTimesForDay($date, $club, $courseId = false)
+    {
+        return $this->_doCurlCall(
+            "https://$club.hub.clubv1.com/Visitors/TeeSheet?date=$date",
+        );
     }
 
     public function getOpens($clubId)
     {
-        $ch = curl_init();
-        curl_setopt_array($ch, [
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_USERAGENT => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36",
-        ]);
-        curl_setopt($ch, CURLOPT_HEADER, true);
-        curl_setopt(
-            $ch,
-            CURLOPT_URL,
+        return $this->_doCurlCall(
             "https://hub.howdidido.com/directory/OpenCompetitions?id=$clubId",
         );
-        $server_output = curl_exec($ch);
-        return preg_replace("/^HTTP\/2\s+200\s.*?\R\R/s", "", $server_output);
     }
 
     public function checkOpenAvailability($token, $openId)
     {
-        $ch = curl_init();
-        curl_setopt_array($ch, [
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_USERAGENT => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36",
-        ]);
-        curl_setopt($ch, CURLOPT_HEADER, true);
-        curl_setopt(
-            $ch,
-            CURLOPT_URL,
+        return $this->_doCurlCall(
             "https://howdidido-whs.clubv1.com/hdidbooking/open?token=$token&cid=$openId&rd=1",
         );
-        $server_output = curl_exec($ch);
-        return preg_replace("/^HTTP\/2\s+200\s.*?\R\R/s", "", $server_output);
     }
 }
